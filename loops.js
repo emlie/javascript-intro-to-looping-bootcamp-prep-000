6f5e197a062485fb9467172a0079965fb0a305c8
Mia Elisenberg <mi-ya@live.no>
2018-10-05 17:38:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i <= 25; i++) {
-    array[i]
+    if (i === 1) {
+      console.log(`I am ${i} strange loop.`);
+    } else {
+      console.log(`I am ${i} strange loops.`);
+    }
   }
 }
